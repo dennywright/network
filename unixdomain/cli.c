ac4e7e37437b99f107e2a38190add4d7a8306d6c
Denny Wright <dwright@gogoair.com>
2015-09-14 17:06:36 -0600

    Use socket activation with systemd
    Compile with systemd
    Include sd-daemon.h
    Call sd_listen_fds to see if socket is active
    otherwise create it

--- unixdomain/cli.c
+++ unixdomain/cli.c
@@ -1,42 +1,62 @@
+#include <systemd/sd-daemon.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+char *socket_path = "/run/srv.sk";
 //char *socket_path = "./socket";
-char *socket_path = "\0hidden";
+//char *socket_path = "\0hidden";
 
-int main(int argc, char *argv[]) {
-  struct sockaddr_un addr;
-  char buf[100];
-  int fd,rc;
+int main(int argc, char *argv[])
+{
+    char buf[100];
+    int n,fd;
 
-  if (argc > 1) socket_path=argv[1];
-
-  if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-    perror("socket error");
-    exit(-1);
-  }
-
-  memset(&addr, 0, sizeof(addr));
-  addr.sun_family = AF_UNIX;
-  strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
-
-  if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    perror("connect error");
-    exit(-1);
-  }
-
-  while( (rc=read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-    if (write(fd, buf, rc) != rc) {
-      if (rc > 0) fprintf(stderr,"partial write");
-      else {
-        perror("write error");
-        exit(-1);
-      }
+    n = sd_listen_fds(0);
+    if (n > 1)
+    {
+        perror("Too many file descriptors received.\n");
+        exit(1);
     }
-  }
+    else if (n == 1)
+    {
+        fd = SD_LISTEN_FDS_START + 0;
+    }
+    else
+    {
+        struct sockaddr_un addr;
 
-  return 0;
+        if (argc > 1) socket_path=argv[1];
+
+        if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+            perror("socket error");
+            exit(-1);
+        }
+
+        memset(&addr, 0, sizeof(addr));
+        addr.sun_family = AF_UNIX;
+        strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+
+        if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+            perror("connect error");
+            exit(-1);
+        }
+
+    }
+
+    int rc;
+
+    while( (rc=read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+        if (write(fd, buf, rc) != rc) {
+            if (rc > 0) fprintf(stderr,"partial write");
+            else {
+                perror("write error");
+                exit(-1);
+            }
+        }
+    }
+
+    return 0;
 }
--- unixdomain/srv.c
+++ unixdomain/srv.c
@@ -1,59 +1,79 @@
+#include <systemd/sd-daemon.h>
 #include <stdio.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <stdlib.h>
 
+char *socket_path = "/run/srv.sk";
 //char *socket_path = "./socket";
-char *socket_path = "\0hidden";
+//char *socket_path = "\0hidden";
 
-int main(int argc, char *argv[]) {
-  struct sockaddr_un addr;
-  char buf[100];
-  int fd,cl,rc;
+int main(int argc, char *argv[])
+{
+    char buf[100];
+    int n,fd;
 
-  if (argc > 1) socket_path=argv[1];
+    n = sd_listen_fds(0);
+    if (n > 1)
+    {
+        perror("Too many file descriptors received.\n");
+        exit(1);
+    }
+    else if (n == 1)
+    {
+        fd = SD_LISTEN_FDS_START + 0;
+    }
+    else
+    {
+        struct sockaddr_un addr;
 
-  if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-    perror("socket error");
-    exit(-1);
-  }
+        if (argc > 1) socket_path=argv[1];
 
-  memset(&addr, 0, sizeof(addr));
-  addr.sun_family = AF_UNIX;
-  strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+        if ( (fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+            perror("socket error");
+            exit(-1);
+        }
 
-  unlink(socket_path);
+        memset(&addr, 0, sizeof(addr));
+        addr.sun_family = AF_UNIX;
+        strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
 
-  if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    perror("bind error");
-    exit(-1);
-  }
+        unlink(socket_path);
 
-  if (listen(fd, 5) == -1) {
-    perror("listen error");
-    exit(-1);
-  }
+        if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+            perror("bind error");
+            exit(-1);
+        }
 
-  while (1) {
-    if ( (cl = accept(fd, NULL, NULL)) == -1) {
-      perror("accept error");
-      continue;
+        if (listen(fd, 5) == -1) {
+            perror("listen error");
+            exit(-1);
+        }
     }
 
-    while ( (rc=read(cl,buf,sizeof(buf))) > 0) {
-      printf("read %u bytes: %.*s\n", rc, rc, buf);
-    }
-    if (rc == -1) {
-      perror("read");
-      exit(-1);
-    }
-    else if (rc == 0) {
-      printf("EOF\n");
-      close(cl);
-    }
-  }
+    while (1) {
+        int cl,rc;
 
+        if ( (cl = accept(fd, NULL, NULL)) == -1) {
+            perror("accept error");
+            continue;
+        }
 
-  return 0;
+        printf("client connected\n");
+    
+        while ( (rc=read(cl,buf,sizeof(buf))) > 0) {
+            printf("read %u bytes: %.*s\n", rc, rc, buf);
+        }
+        if (rc == -1) {
+            perror("read");
+            exit(-1);
+        }
+        else if (rc == 0) {
+            printf("EOF\n");
+            close(cl);
+        }
+    }
+
+    return 0;
 }
 
